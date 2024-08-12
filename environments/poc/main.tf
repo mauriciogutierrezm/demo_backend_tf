daf1efdf11699beef20893b9e19165faa385db89
Mauricio Eduardo Gutiérrez Martínez <mauricio.gutierrez@falp.org>
2024-08-12 16:58:53 -0400

    Revert "fix source ssh"
    This reverts commit 464feb6bc42ce746bd6fa63188d3fe1f13099833.

--- environments/poc/main.tf
+++ environments/poc/main.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source               = "git::ssh://git@github.com:mauriciogutierrezm/demo_modules_tf.git//backend"
+  source               = "git::ssh://git@github.com:mauriciogutierrezm/demo_modules_tf.git//backend?ref=develop"
   terraform_state_name = var.terraform_state_name
   tags                 = var.tags
 }
