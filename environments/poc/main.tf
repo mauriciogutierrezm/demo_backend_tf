4be7919066a606a623e05a9bff31aa1d22dbcb52
Mauricio Eduardo Gutiérrez Martínez <mauricio.gutierrez@falp.org>
2024-08-12 16:59:15 -0400

    fix source ssh

--- environments/poc/main.tf
+++ environments/poc/main.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source               = "git::ssh://git@github.com:mauriciogutierrezm/demo_modules_tf.git//backend?ref=develop"
+  source               = "git::ssh://git@github.com:mauriciogutierrezm/demo_modules_tf.git//backend"
   terraform_state_name = var.terraform_state_name
   tags                 = var.tags
 }
