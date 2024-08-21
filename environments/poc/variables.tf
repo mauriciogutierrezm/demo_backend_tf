a9b6077de1023e2222b59fc94f9af4630a5f4b3b
Mauricio Eduardo Gutiérrez Martínez <mauricio.gutierrez@falp.org>
2024-08-20 22:57:29 -0400

    fix env vars name, and change name of the state to lambda-terraform-state-poc

--- environments/poc/variables.tf
+++ environments/poc/variables.tf
@@ -1,14 +1,14 @@
 variable "terraform_state_name" {
   description = "The name of the S3 bucket to be created for storing Terraform state"
   type        = string
-  default     = "demo-backend-github-terraform-state-poc"
+  default     = "lambda-terraform-state-poc"
 }
 
 variable "tags" {
   description = "Tags for the bucket"
   type        = map(string)
   default = {
-    Project     = "demo-backend-github"
+    Project     = "poc-lambda-tf"
     Environment = "poc"
   }
 }
